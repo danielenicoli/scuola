44a18046e9324eb4dc756f8978191ee685942d55
Daniele Nicoli <daniele@nicoli.dev>
2024-01-16 22:08:49 +0100

    Versione base client e server FTP con socket TCP in C#

---
+++ Socket/Console FTP/ClientFTP/ClientFTP/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+class TCPClient
+{
+    static void Main()
+    {
+        TcpClient client = null;
+
+        try
+        {
+            string ipAddress = "127.0.0.1";
+            int port = 2121;
+
+            client = new TcpClient(ipAddress, port);
+            Console.WriteLine("Connesso al server su {0}:{1}", ipAddress, port);
+
+            NetworkStream stream = client.GetStream();
+
+            byte[] buffer = new byte[1024];
+
+            while (true)
+            {
+                Console.Write("\nInserisci un comando (GetList, ReadFile, Exit per terminare): ");
+                string comando = Console.ReadLine();
+
+                // Invio comando al server
+                byte[] comandoBuffer = Encoding.ASCII.GetBytes(comando);
+                stream.Write(comandoBuffer, 0, comandoBuffer.Length);
+
+                // Ricezione risposta dal server
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                string response = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                Console.Clear();
+                Console.WriteLine($"Risposta dal server {ipAddress}:{port} \n{response}");
+
+                if (comando.ToLower() == "exit")
+                    break;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Errore: {0}", e.Message);
+        }
+        finally
+        {
+            // Chiudi la connessione
+            client.Close();
+        }
+    }
+}
---
+++ Socket/Console FTP/ServerFTP/ServerFTP/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class TCPServer
+{
+    static void Main()
+    {
+        TcpListener server = null;
+
+        try
+        {
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            int port = 2121;
+
+            server = new TcpListener(ipAddress, port);
+
+            server.Start();
+            Console.WriteLine("Server in ascolto su {0}:{1}", ipAddress, port);
+
+            TcpClient client = server.AcceptTcpClient();
+            Console.WriteLine("Client connesso!");
+
+            NetworkStream stream = client.GetStream();
+
+            byte[] buffer = new byte[1024];
+
+            int bytesRead;
+            while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                string comando = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                Console.WriteLine("Comando ricevuto: {0}", comando);
+
+                // Esegui comando e invia risposta al client
+                string response = EseguiComando(comando);
+                byte[] responseBuffer = Encoding.ASCII.GetBytes(response);
+                stream.Write(responseBuffer, 0, responseBuffer.Length);
+
+                // Se il comando è "exit" termina il ciclo di lettura
+                if (comando.Trim().ToLower() == "exit")
+                    break;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Errore: {0}", e.Message);
+        }
+        finally
+        {
+            server.Stop();
+        }
+    }
+
+    static string EseguiComando(string comando)
+    {
+        if (comando.ToLower() == "getlist")
+        {
+            string[] files = Directory.GetFiles(Directory.GetCurrentDirectory());
+            return string.Join("\n", files);
+        }
+        else if (comando.ToLower().StartsWith("readfile"))
+        {
+            // Estrazione nome del file dalla sintassi ReadFile [nomeFile]
+            string fileName = comando.Substring("readfile".Length).Trim();
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+
+            if (File.Exists(filePath))
+            {
+                string fileContent = File.ReadAllText(filePath);
+                return fileContent;
+            }
+            else
+            {
+                return $"Il file '{filePath}' non esiste.";
+            }
+        }
+        else
+        {
+            return "Comando sconosciuto";
+        }
+    }
+}

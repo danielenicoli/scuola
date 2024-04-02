daa42dae12960c9bafad88b0a30c8948781e3793
Daniele Nicoli <daniele@nicoli.dev>
2024-04-02 15:28:50 +0200

    Todo REST API con ASP.NET Core

---
+++ Web service e API REST/ASPNET_Core_Todo_Api/TodoApi/Models/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿using TodoApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.AspNetCore.Http.HttpResults;
+namespace TodoApi.Models;
+
+public class TodoItem
+{
+    public long Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+}
